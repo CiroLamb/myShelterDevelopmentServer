e1ff6b775ebbeacbc2796843e3eabe50667b2a42
ibrahim855 <78034840+ibrahim855@users.noreply.github.com>
2022-03-25 16:11:18 +0100

    adding route for getting all elements

--- app.js
+++ app.js
@@ -7,60 +7,69 @@ const app = express();
 
 const PORT = process.env.PORT || 8081;
 
-const elements = require('./data.js');
-
+const elements = require("./data.js");
 
 // CORS POLICY
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', "*");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Authorization, X-Requested-With, Content-Type, Accept');
-    res.type('application/json');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, POST");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-Requested-With, Content-Type, Accept"
+  );
+  res.type("application/json");
+  next();
 });
 
 function getElementById(id) {
-    const elementFiltering = [];
-    elements.forEach(element => {
-        if(element["$"].id_group === id) {
-            elementFiltering.push(element["$"]);
-        }
-    });
+  const elementFiltering = [];
+  elements.forEach((element) => {
+    if (element["$"].id_group === id) {
+      elementFiltering.push(element["$"]);
+    }
+  });
 
-    // convert to xml
-    return elementFiltering;
-};
-
+  // convert to xml
+  return elementFiltering;
+}
 
 // FIXME: Update element randomly
 
-app.get("/element_status/:id", (req,res) => {
-    const { id } = req.params;
+app.get("/element_status/:id", (req, res) => {
+  const { id } = req.params;
+
+  // FIXME: DO SOMETHING HERE
+  // fs.readFile(path.resolve(__dirname, "data.xml"), (error, result) => {
+  //     console.log(result);
+  //     if(!error) {
+  //         // res.status(200).json({
+  //         //     elements: getElementById(id)
+  //         // })
+  //         // res.send(xml(getElementById(id)));
+  //         res.status(200).send(xml(result));
+  //     };
+
+  // });
 
 
-    // FIXME: DO SOMETHING HERE
-    // fs.readFile(path.resolve(__dirname, "data.xml"), (error, result) => {
-    //     console.log(result);
-    //     if(!error) {
-    //         // res.status(200).json({
-    //         //     elements: getElementById(id)
-    //         // })
-    //         // res.send(xml(getElementById(id)));
-    //         res.status(200).send(xml(result));
-    //     };
 
-    // });
-
-    res.status(200).json({
-        elements: getElementById(id),
-    })
-    
-
-    
+  res.status(200).json({
+    elements: getElementById(id),
+  });
 });
 
+app.get("/element_status", (req, res) => {
+    const myElements = [];
+    elements.forEach(element => {
+      myElements.push(element["$"]);
+    });
+
+  res.status(200).json({
+    elements: myElements
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is Listening on PORT: ${PORT}`);
+  console.log(`Server is Listening on PORT: ${PORT}`);
 });
